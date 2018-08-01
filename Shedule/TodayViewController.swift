52fcb284f4901284631ebbab0c2b4fa9ca0f8cf9
Unknown <sapegina1998@inbox.ru>
2018-08-01 14:00:09 +0300

    Widget doing

--- Shedule/TodayViewController.swift
+++ Shedule/TodayViewController.swift
@@ -2,7 +2,7 @@
 //  TodayViewController.swift
 //  Shedule
 //
-//  Created by Arthur on 31.07.2018.
+//  Created by Kate on 31.07.2018.
 //  Copyright © 2018 Руднев Кирилл. All rights reserved.
 //
 
@@ -17,16 +17,15 @@ class TodayViewController: UIViewController{
         self.tableView.reloadData()
         if sender.isSelected{
             self.tableViewHeightConstraint?.constant = cell_height
-            lessons[0] = String(Int(cell_height))
             sender.isSelected = false
         }
         else{
             self.tableViewHeightConstraint?.constant = cell_height * CGFloat(self.lessons.count)
-            lessons[0] = String(Int(cell_height * CGFloat(self.lessons.count)))
             sender.isSelected = true
         }
     }
-    var lessons = ["Lesson 1", "Lesson 2", "Lesson 3", "Lesson 4", "Lesson 5", "Lesson 6", "Lesson 7"]
+    var lessons = ["Алгебра", "Английский язык", "Испанский язык", "Физическая культура", "География", "Лингвистика", "Информатика"]
+    var times = ["08:00 - 08:45", "09:00 - 09:45", "10:00 - 10:45", "11:00 - 11:45", "12:00 - 12:45", "13:00 - 13:45", "14:00 - 14:45"]
     let cell_height: CGFloat = 36
     private var tableViewHeightConstraint : NSLayoutConstraint?
     
@@ -51,14 +50,11 @@ class TodayViewController: UIViewController{
 
 extension TodayViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("Блядь")
         return lessons.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellID = "Cell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        cell.textLabel?.text = lessons[indexPath.row]
+        let cell = CustomCell(lesson: lessons[indexPath.row], time_of_lesson: times[indexPath.row], width: tableView.frame.width, height: cell_height, margin: 15)
         return cell
     }
     
@@ -85,5 +81,38 @@ extension TodayViewController: NCWidgetProviding{
         else{
             preferredContentSize = maxSize
         }
+    }
+}
+
+
+class CustomCell: UITableViewCell {
+    var lesson_label: UILabel!
+    var time_label: UILabel!
+    
+    init(lesson: String, time_of_lesson: String, width: CGFloat, height: CGFloat, margin: CGFloat) {
+        super.init(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        lesson_label = UILabel(frame: CGRect(x: margin, y: 0, width: width*0.6-margin, height: height))
+        time_label = UILabel(frame: CGRect(x: width*0.6, y: 0, width: width*0.4-margin, height: height))
+        lesson_label.textAlignment = .left
+        time_label.textAlignment = .right
+        lesson_label.text = lesson
+        time_label.text = time_of_lesson
+        if Int(width) < 320{
+            lesson_label.font = UIFont(name: "HelveticaNeue", size: 14)
+            time_label.font = UIFont(name: "HelveticaNeue-Light", size:  14)
+        }else{
+            lesson_label.font = UIFont(name: "HelveticaNeue", size: 16)
+            time_label.font = UIFont(name: "HelveticaNeue-Light", size:  16)
+        }
+        addSubview(lesson_label)
+        addSubview(time_label)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
 }
